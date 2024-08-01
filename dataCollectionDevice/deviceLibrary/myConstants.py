ea5cfeacce0af6ec0d9bbbc47ecd0ced79ad792c
Steffan Lund Franch <steffan.svendsen@gmail.com>
2024-08-01 15:54:49 +0200

    hide passwords

--- dataCollectionDevice/deviceLibrary/myConstants.py
+++ dataCollectionDevice/deviceLibrary/myConstants.py
@@ -4,10 +4,10 @@ class myConstants:
     host = "localhost"
     user = "root"
     database = "mydatabase"
-    password_windows = "Dino1@dlgnet.dk"
-    password_linux = "Svendsen92//"
-    dataTableName = "dataTable"
-    webTablename = "webInterfaceTable"
-    sensorTableName = "sensTable"
+    password_windows = "xxxxxx"
+    password_linux = "xxxxxx"
+    dataTableName = "dataCollection_table"
+    webTablename = "webInterface_table"
+    sensorTableName = "sensor_table"
 
     sensorType : list = ["temperature", "humidity", "light"]
