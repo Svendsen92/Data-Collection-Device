772edb128610026bfe306fe135f50a99c079f78e
Steffan Lund Franch <steffan.lund.franch@gmail.com>
2024-08-02 18:00:14 +0200

    uploaded Library code

---
+++ deviceLibrary/myConstants.py
@@ -0,0 +1,13 @@
+
+class myConstants:
+
+    host = "localhost"
+    user = "root"
+    database = "mydatabase"
+    password_windows = "Dino1@dlgnet.dk"
+    password_linux = "Svendsen92//"
+    dataTableName = "dataCollection_table"
+    webTablename = "webInterface_table"
+    sensorTableName = "sensor_table"
+
+    sensorType : list = ["temperature", "humidity", "light"]
