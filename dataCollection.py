858b0669634b18035cda7ce8da4e5ff5f0bbce81
Steffan Lund Franch <steffan.lund.franch@gmail.com>
2024-08-20 11:19:02 +0200

    added Logging

--- dataCollection.py
+++ dataCollection.py
@@ -4,6 +4,7 @@ import socket
 import platform
 import calendar
 import statistics
+import logging
 
 from datetime import datetime
 from deviceLibrary.mySQL_DatabaseLib import mySQL_DatabaseLib # type: ignore
@@ -12,12 +13,25 @@ from deviceLibrary.myConstants import myConstants as const # type: ignore
 
 if platform.system() == "Windows":
     password = const.password_windows # host PC's password
+    from random import uniform
 elif platform.system() == "Linux":
     password = const.password_linux # edgeDevice password
     import Adafruit_DHT # type: ignore
 else:
     print("OS is not supported")
     exit()
+
+
+# Create and configure logger
+logging.basicConfig(filename="dataCollection_logFile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.INFO)
 
 
 ######################################
@@ -32,6 +46,7 @@ def get_myLocal_ip() -> str:
         IP = '127.0.0.1'
     finally:
         s.close()
+        logger.info(f"get_myLocal_ip = {IP}")
     return IP
 
 def getInputArguments():
@@ -111,10 +126,12 @@ def dataCollection(db: mySQL_DatabaseLib) -> None:
             sensorConfig.clear()
             for sensorType in const.sensorTypeList:
                 """ Retrieve if the sensor is activated or not"""
-                is_sensorActivated.append(db.select(tableName=const.sensorTableName, header="is_active", condition=f"sensorType = '{sensorType}'")[0][0])
+                result = db.select(tableName=const.sensorTableName, header="is_active", condition=f"sensorType = '{sensorType}'")['result']
+                is_sensorActivated.append(result[0][0])
 
                 """ Retrieve the different read & update interval for each sensor, if None set to default value """
-                sensorConfig.append(list(db.select(tableName=const.sensorTableName, header="updateInterval, readInterval", condition=f"sensorType = '{sensorType}'")[0]))
+                result = db.select(tableName=const.sensorTableName, header="updateInterval, readInterval", condition=f"sensorType = '{sensorType}'")['result']
+                sensorConfig.append(list(result[0]))
                 # Check that database update interval has been set, else set it to default value
                 if sensorConfig[len(sensorConfig) -1][0] == None:
                     sensorConfig[len(sensorConfig) -1][0] = 1800 # default value 15 minuts
@@ -134,27 +151,26 @@ def dataCollection(db: mySQL_DatabaseLib) -> None:
                 else:
                     DHT_data = {"temp":0, "humidity":0, "GoodRead":False}
                     DHT_data['GoodRead'] = True
-                    DHT_data['temp'] = 14.2
+                    DHT_data['temp'] = uniform(-20, 80) # Outputs a random float between 1 and 10.
 
                 if (DHT_data['GoodRead']):
                     sensorUpdateTimer[temperatureIdx] = getSecondsSinceEpoch()
                     temperatureList.append(DHT_data["temp"])
-                    print("Temp=" + str(DHT_data['temp']) + "ºC, Humidity=" + str(DHT_data['humidity']))
 
             # Sensor Database update interval in seconds
             if (getSecondsSinceEpoch() - databaseUpdateTimer[temperatureIdx] >= sensorConfig[temperatureIdx][updateIntervalIdx]):
                 databaseUpdateTimer[temperatureIdx] = getSecondsSinceEpoch()
-
                 try:
                     # Update temperature in database
                     timeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     avgTemperature = statistics.median(temperatureList) 
                     temperatureList.clear()  
                     rowValues = ("Temperature", avgTemperature, timeStamp)
-                    db.insert(tableName=const.dataTableName, headerStr="inputType, val, timeStamp", values=rowValues)
-                    print(f"Temp={avgTemperature}ºC, timeStamp={timeStamp}")
+                    result = db.insert(tableName=const.dataTableName, header=["inputType", "value", "timeStamp"], values=rowValues)
+                    logger.info(result['message'])
+                    logger.info(f"Temp={avgTemperature}ºC, timeStamp={timeStamp}")
                 except Exception as error:
-                    print(error)
+                    logger.error(error)
                     
 
         """Humidity Sensor reading and Database updating"""
@@ -166,27 +182,26 @@ def dataCollection(db: mySQL_DatabaseLib) -> None:
                 else:
                     DHT_data = {"temp":0, "humidity":0, "GoodRead":False}
                     DHT_data['GoodRead'] = True
-                    DHT_data['humidity'] = 45.4
+                    DHT_data['humidity'] = uniform(20, 100) # Outputs a random float between 1 and 10.
 
                 if (DHT_data['GoodRead']):
                     sensorUpdateTimer[humidityIdx] = getSecondsSinceEpoch()
                     humidityList.append(DHT_data["humidity"])
-                    print("Temp=" + str(DHT_data['temp']) + "ºC, Humidity=" + str(DHT_data['humidity']))
 
             # Sensor Database update interval in seconds
             if (getSecondsSinceEpoch() - databaseUpdateTimer[humidityIdx] >= sensorConfig[humidityIdx][updateIntervalIdx]): 
                 databaseUpdateTimer[humidityIdx] = getSecondsSinceEpoch()
-                    
                 try:
                     # Update humidity in database
                     timeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     avgHumidity =  statistics.median(humidityList)
                     humidityList.clear()
                     rowValues = ("Humidity", avgHumidity, timeStamp)
-                    db.insert(tableName=const.dataTableName, headerStr="inputType, val, timeStamp", values=rowValues)
-                    print(f"Humidity={avgHumidity}, timeStamp={timeStamp}")
+                    result = db.insert(tableName=const.dataTableName, header=["inputType", "value", "timeStamp"], values=rowValues)
+                    logger.info(result['message'])
+                    logger.info(f"Humidity={avgHumidity}, timeStamp={timeStamp}")
                 except Exception as error:
-                    print(error)
+                    logger.error(error)
 
 
         """Light Sensor reading and Database updating"""
@@ -206,10 +221,11 @@ def dataCollection(db: mySQL_DatabaseLib) -> None:
                     avgLight =  statistics.median(lightList)
                     lightList.clear()
                     rowValues = ("Light", avgLight, timeStamp)
-                    db.insert(tableName=const.dataTableName, headerStr="inputType, val, timeStamp", values=rowValues)
-                    print(f"Light={avgLight}, timeStamp={timeStamp}")
+                    db.insert(tableName=const.dataTableName, header=["inputType", "value", "timeStamp"], values=rowValues)
+                    logger.info(result['message'])
+                    logger.info(f"Light={avgLight}, timeStamp={timeStamp}")
                 except Exception as error:
-                    print(error)
+                    logger.error(error)
 
 
 
@@ -219,33 +235,37 @@ if __name__ =="__main__":
 
     """ Create database object """
     db = mySQL_DatabaseLib(host=const.host, user=const.user, password=password, database=const.database)
-    db.createDatabase(const.database) # Create database, if it does not already exist
-
+    result = db.createDatabase(const.database) # Create database, if it does not already exist
+    logger.info(f"INFO : {result['message']}")
 
     """ Create dataCollection_table, if it does not already exist """
-    ColumnNames = {"inputType": "VARCHAR(20)", "val": "FLOAT", "timeStamp": "DATETIME"}
-    status = db.createTable(tableName=const.dataTableName, columnHeaders=ColumnNames)
+    ColumnNames = {"inputType": "VARCHAR(20)", "value": "FLOAT", "timeStamp": "DATETIME"}
+    result = db.createTable(tableName=const.dataTableName, columnHeaders=ColumnNames)
+    logger.info(f"INFO : {result['message']}")
 
     """ Create webInterface_table, if it does not already exist """
     ColumnNames = {"deviceIP": "VARCHAR(15)", "deviceName": "VARCHAR(75)", "timeStamp": "DATETIME"}
-    if db.createTable(tableName=const.webTablename, columnHeaders=ColumnNames):
+    result = db.createTable(tableName=const.webTableName, columnHeaders=ColumnNames)
+    logger.info(f"INFO : {result['message']}")
+    if result['status']:
         header = ["deviceIP", "deviceName", "timeStamp"]
         timeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         values: tuple = (get_myLocal_ip(), "RPi1", timeStamp)
-        status = db.insert(tableName=const.webTablename, header=header, values=values)
-        print(f"webInterface_table : Insert status = {status}")
+        result = db.insert(tableName=const.webTableName, header=header, values=values)
+        logger.info(f"INFO : {result['message']}")
 
 
     """ Create sensor_table, if it does not already exist """
     ColumnNames = {"sensorType": "VARCHAR(20)", "is_active": "BOOL", "updateInterval": "INT", "readInterval": "INT", "timeStamp": "DATETIME"}
-    if db.createTable(tableName=const.sensorTableName, columnHeaders=ColumnNames):
+    result = db.createTable(tableName=const.sensorTableName, columnHeaders=ColumnNames)
+    logger.info(f"INFO : {result['message']}")
+    if result['status']:
         header = ["sensorType", "is_active", "readInterval", "updateInterval", "timeStamp"]
         timeStamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         values: tuple = (("temperature", False, 10, 1800, timeStamp), ("humidity", False, 10, 1800, timeStamp), ("light", False, 10, 1800, timeStamp))
         for valueSet in values:
-            status = db.insert(tableName=const.sensorTableName, header=header, values=valueSet)
-
-        print(f"sensor_table : Insert status = {status}")
+            result = db.insert(tableName=const.sensorTableName, header=header, values=valueSet)
+            logger.info(f"INFO : {result['message']}")
 
 
 
